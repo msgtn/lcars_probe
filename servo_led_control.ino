8ba25fcd2e687c0180973e7e8a328b0faa1ec0c3
Mike <psychomugs@gmail.com>
2016-06-22 22:06:43 -0400

    Updated arduino sketches

--- servo_led_control.ino
+++ servo_led_control.ino
@@ -1,12 +1,10 @@
 #include <Servo.h>
 
-#define led_pin_1 7
-#define led_pin_2 8
-#define led_pin_3 9
-#define led_pin_4 10
-#define led_pin_5 11
-#define led_pin_6 12
-#define led_pin_7 13
+#define led_pin_1 8
+#define led_pin_2 9
+#define led_pin_3 10
+#define led_pin_4 11
+#define led_pin_5 12
 
 #define srv_pin_fr 2
 #define srv_pin_fl 3
@@ -24,10 +22,13 @@
 #define fr_open  175
 #define fl_close 170
 #define fl_open  100
-#define br_close 100
-#define br_open  170
-#define bl_close 175
-#define bl_open  105
+#define br_close 170
+#define br_open  100
+#define bl_close 105
+#define bl_open  175
+#define sp_cw    180
+#define sp_ccw   0
+#define sp_stop  95
 
 Servo servo_fr;
 Servo servo_fl;
@@ -37,7 +38,7 @@ Servo servo_sp;
 
 void setup() {
   Serial.begin(9600);
-  for (int i = led_pin_1; i <= led_pin_7; i++) {
+  for (int i = led_pin_1; i <= led_pin_5; i++) {
     pinMode(i, OUTPUT);
     digitalWrite(i, LOW);
   }
@@ -89,14 +90,6 @@ void loop() {
       digitalWrite(led_pin_5, HIGH);
     } else if (c == 'g') {
       digitalWrite(led_pin_5, LOW);
-    } else if (c == 'y') {
-      digitalWrite(led_pin_6, HIGH);
-    } else if (c == 'h') {
-      digitalWrite(led_pin_6, LOW);
-    } else if (c == 'u') {
-      digitalWrite(led_pin_7, HIGH);
-    } else if (c == 'j') {
-      digitalWrite(led_pin_7, LOW);
     } else if (c == 'i') {
       servo_fr.write(fr_open);
     } else if (c == 'k') {
@@ -113,6 +106,12 @@ void loop() {
       servo_bl.write(bl_open);
     } else if (c == '\'') {
       servo_bl.write(bl_close);
+    } else if (c == 'z') {
+      servo_sp.write(sp_cw);
+    } else if (c == 'x') {
+      servo_sp.write(sp_stop);
+    } else if (c == 'c') {
+      servo_sp.write(sp_ccw);
     }
   }
 }
